679a836228ab02447687aaf4de5605ce6611bb9b
Ryan <ryan.l.smith4@gmail.com>
2019-09-26 14:53:17 -0700

    [ADD] Finsihs iniial challenges

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 
-const { random, randomD, randomRolls } = require('./utils')
+const { randomInt, randomD, randomRolls } = require('./utils')
 
 const app = express()
 
@@ -28,7 +28,16 @@ app.get('/about', (req, res) => {
 // Where n=99 sets the range of the random number returned
 app.get('/random', (req, res) => {
   const { n } = req.query
-  const value = random(n)
+  const value = randomInt(n)
+  res.json({ value })
+})
+
+app.get('/randomD', (req, res) => {
+  const { n } = req.query;
+  if (n <= 1) {
+    res.json('Must be More than 1')
+  }
+  const value = randomD(n)
   res.json({ value })
 })
 
@@ -37,6 +46,27 @@ app.get('/randomrolls', (req, res) => {
   const { n, s } = req.query
   const rolls = randomRolls(n, s)
   res.json({ rolls }) // { "rolls": [1,2,3] }
+})
+
+app.get('/random/:d', (req, res) => {
+  const { d } = req.params
+  const value = randomInt(d)
+  res.json({ value })
+})
+
+app.get('/random/die/:d', (req, res) => {
+  const { d } = req.params
+  if (d <= 1) {
+    res.json('Must be More than 1')
+  }
+  const value = randomD(d)
+  res.json({ value })
+})
+
+app.get('/random/dice/:n/:s', (req, res) => {
+  const { n, s } = req.params
+  const rolls = randomRolls(n, s)
+  res.json({ rolls })
 })
 
 const port = 4000
--- utils.js
+++ utils.js
@@ -1,19 +1,24 @@
 /* eslint-disable semi */
+const { random, round, floor } = Math
 
-function random(n) {
-  return Math.random() * n
+function randomInt(n) {
+  return floor(random() * n) // 1 to n - 1 
 }
 
 function randomD(n) {
-  
-  return 0
+  // n = 6
+  const ranNum = randomInt(n) + 1
+  return ranNum
 }
 
 function randomRolls(n, s) {
-
-  return []
+  const randomNumbers = []
+  for (let i = 0; i <= n - 1; i += 1) {
+    randomNumbers.push(randomD(s))
+  }
+  return randomNumbers
 }
 
-module.exports.random = random
+module.exports.randomInt = randomInt
 module.exports.randomD = randomD
 module.exports.randomRolls = randomRolls
